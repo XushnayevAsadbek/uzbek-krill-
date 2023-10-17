2199813ec0182fc509251927b314e68e10b25637
XushnayevAsadbek <xushnaevasadbek@gmail.com>
2023-10-18 00:09:49 +0500

    intial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,123 @@
-import Image from 'next/image'
+'use client'
+import { useState } from 'react';
+import './globals.css'
 
 export default function Home() {
+  const [lotinText, setLotinText] = useState('');
+  const [krillText, setKrillText] = useState('');
+  const [textteskariText, settextteskariText] = useState('');
+
+  const translateToKrill = () => {
+    const translationMap = {
+      'a': 'а',
+      'A': 'A',
+      'b': 'б',
+      'B': 'Б',
+      'd': 'д',
+      'D': 'Д',
+      'E': 'E',
+      'e': 'e',
+      'F': 'Ф',
+      'f': 'ф',
+      'G': 'Г',
+      'g': 'г',
+      'H': 'Ҳ',
+      'h': 'ҳ',
+      'I': 'И',
+      'i': 'и',
+      'J': 'Ж ',
+      'j': 'ж',
+      'K': 'К',
+      'k': 'к',
+      'L': 'Л',
+      'l': 'л',
+      'M': 'М',
+      'm': 'м',
+      'N': 'Н',
+      'n': 'н',
+      'O': 'О',
+      'o': 'о',
+      'P': 'П',
+      'p': 'п',
+      'Q': 'Қ',
+      'q': 'қ',
+      'R': 'Р',
+      'r': 'р',
+      'S': 'C',
+      's': 'c',
+      'T': 'Т',
+      't': 'т',
+      'U': 'У',
+      'u': 'у',
+      'V': 'В',
+      'v': 'в',
+      'X': 'X',
+      'x': 'x',
+      'Y': 'Й',
+      'y': 'й',
+      'Z': 'З',
+      'z': 'з',
+      'SH': 'Ш',
+      'sh': 'ш',
+    };
+
+    const translatedText = lotinText
+      .split('')
+      .map((char) => translationMap[char] || char)
+      .join('');
+    reverseString(lotinText)
+    setKrillText(translatedText);
+  };
+
+  function reverseString(str) {
+    var splitString = str.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    settextteskariText(joinArray);
+
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-12">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Lotin-Krill translator </h1>
+          </div>
+          <div className="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
+            <div className="relative flex-grow w-full">
+              <label htmlFor="text" className="leading-7 text-sm text-gray-600">Uzbek Latin</label>
+              <textarea
+                value={lotinText}
+                onChange={(e) => setLotinText(e.target.value)}
+                placeholder="Matnni kiriting..."
+                id="text" name="text" className="w-full h-96 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            </div>
+            <div className="relative flex-grow w-full">
+              <label htmlFor="text" className="leading-10 text-sm text-gray-600">Uzbek Krill</label>
+              <textarea
+                value={krillText}
+                readOnly
+                placeholder="Matnning kirilchasi..."
+                id="text" name="text" className="w-full h-96 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            </div>
+            <div className="relative flex-grow w-full">
+              <label htmlFor="text" className="leading-10 text-sm text-gray-600">Uzbek Latin teskarisi</label>
+              <textarea
+                value={textteskariText}
+                readOnly
+                placeholder="Matnning teskari..."
+                id="text" name="text" className="w-full h-96 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+
+              </textarea>
+            </div>
+          </div>
+          <button className="w-auto flex flex-col m-auto justify-center text-white  bg-indigo-500 border-0 py-2 px-10 mt-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" onClick={translateToKrill}>Natija</button>
+
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   )
 }
